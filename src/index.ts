5307e0b7cf81a04c15b9dd341a135e8147137075
D <dhy@jshxlsp.com>
2023-01-12 15:22:18 +0800

    feat: 导出context

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { NestedListView } from './nested-list-view';
 import { NestedRow } from './nested-row';
 import { Node } from './node-view';
+import { useNodesContext } from './nodes-context-provider';
 
 export { NestedRow };
-
+export { useNodesContext }
 export { Node as INode };
 
 export default NestedListView;
